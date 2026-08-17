49c4403c44fdb4a9619560f729866dc27dc938b7
ijfvs <ijfvsenosa@mymail.mapua.edu.ph>
2026-08-17 12:38:39 +0800

    Delete Optimizer directory

--- Optimizer/run_dashboard.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title CAPELCO Power Dispatch Optimizer
-echo ============================================================
-echo Starting CAPELCO Power Dispatch Optimizer Web Dashboard...
-echo ============================================================
-python -m streamlit run app.py
-pause
